2af74e159ad7fe98119dd3ec7e20a4a0248a2bac
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-12-24 20:31:00 +0100

    new commit

---
+++ compte-cqrs-event-sourcing/src/test/java/com/lamkirich/comptecqrseventsourcing/CompteCqrsEventSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamkirich.comptecqrseventsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompteCqrsEventSourcingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
