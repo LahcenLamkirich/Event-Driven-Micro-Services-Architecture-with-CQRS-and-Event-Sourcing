4f2c8d066d6dbc6eb23b4b68ec08c7a33b6aa9fe
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-12-25 14:41:58 +0100

    new commit

--- compte-cqrs-event-sourcing/src/main/java/com/lamkirich/comptecqrseventsourcing/CompteCqrsEventSourcingApplication.java
+++ compte-cqrs-event-sourcing/src/main/java/com/lamkirich/comptecqrseventsourcing/CompteCqrsEventSourcingApplication.java
@@ -1,17 +1,23 @@
 package com.lamkirich.comptecqrseventsourcing;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class CompteCqrsEventSourcingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CompteCqrsEventSourcingApplication.class, args);
+
     }
 
+    @Bean
+    CommandLineRunner start(){
+        return args -> {
+            System.out.println("Hello from the CQRS Application !");
+        };
+    }
 }
