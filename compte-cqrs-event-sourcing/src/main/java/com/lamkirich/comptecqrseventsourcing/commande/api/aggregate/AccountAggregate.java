f12ac5355d5bc38ffe937b71cc26192e9ccad241
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-12-25 15:15:25 +0100

    new commit

---
+++ compte-cqrs-event-sourcing/src/main/java/com/lamkirich/comptecqrseventsourcing/commande/api/aggregate/AccountAggregate.java
@@ -0,0 +1,32 @@
+package com.lamkirich.comptecqrseventsourcing.commande.api.aggregate;
+
+import com.lamkirich.comptecqrseventsourcing.commande.api.commands.CreateAccountCommand;
+import com.lamkirich.comptecqrseventsourcing.commande.api.events.AccountCreatedEvent;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
+
+@Aggregate
+public class AccountAggregate {
+
+    @AggregateIdentifier
+    private String AccountId;
+
+    private String currency;
+    private double initialBalance;
+
+    // Constructor that will inject the CreateAccountCommand
+    public AccountAggregate(CreateAccountCommand createAccountCommand) {
+        // Perform validations:
+        AccountCreatedEvent accountCreatedEvent = new AccountCreatedEvent();
+
+        BeanUtils.copyProperties(createAccountCommand, accountCreatedEvent);
+
+        AggregateLifecycle.apply(accountCreatedEvent);
+    }
+
+    // default Constructor
+    public AccountAggregate() {
+    }
+}
---
+++ compte-cqrs-event-sourcing/src/main/java/com/lamkirich/comptecqrseventsourcing/commande/api/events/AccountCreatedEvent.java
@@ -0,0 +1,14 @@
+package com.lamkirich.comptecqrseventsourcing.commande.api.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountCreatedEvent {
+
+}
